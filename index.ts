1852861cad94efb22f117aedb175fc9949030304
0xlino <13621351+0xlino@users.noreply.github.com>
2024-06-20 19:24:31 +0100

    update

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const injectHeaderContainer = () => {
   return `
          <div class="container">
             <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="/"
+                <a href="/crypto-images"
                     class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                     <svg class="bi me-2" width="40" height="32">
                         <use xlink:href="#bootstrap"></use>
